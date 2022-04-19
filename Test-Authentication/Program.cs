87396aab21875082572b390470343a661930b48b
RudolfAmersfoort <r.amersfoort@tools4ever.com>
2022-04-19 12:29:11 +0200

    scaffolded RegisterPage

--- Test-Authentication/Program.cs
+++ Test-Authentication/Program.cs
@@ -8,6 +8,13 @@ using Test_Authentication.Areas.Identity;
 using Test_Authentication.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
